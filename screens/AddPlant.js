c96bd453ecc2141146fdefc2d6fb9ad083e15d5a
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-07-08 15:04:32 -0700

    added home, listPlants, and addPlant screens to app

---
+++ screens/AddPlant.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AddPlant extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Add New Plant</Text>
+      </View>
+    );
+  }
+}
+
+export default AddPlant;
---
+++ screens/ListPlants.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ListPlants extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Your Plants</Text>
+      </View>
+    );
+  }
+}
+
+export default ListPlants;
