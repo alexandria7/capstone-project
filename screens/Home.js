59189c2cfe8ab63c7e53e983cd064aa2ec5871d2
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-07-08 15:09:45 -0700

    added button to link from home screen to plant list

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 class Home extends Component {
   render() {
     return (
       <View>
         <Text>Home Screen</Text>
+
+        <Button 
+            title="My Plants"
+            onPress={ () => this.props.navigation.navigate('ListPlants') }
+        />
+
       </View>
     );
   }
