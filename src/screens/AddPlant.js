fb81c27ff29e1bb88a012a067d78fc2098bdb7d3
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-07-08 16:39:19 -0700

    added more screen components for basic plant viewing functionality

--- src/screens/AddPlant.js
+++ src/screens/AddPlant.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { firebase } from '../config';
+
+// const { currentUser } = firebase.auth();
+// firebase.database().ref(`/users/${currentUser.uid}/plants`)
 
 class AddPlant extends Component {
   render() {
