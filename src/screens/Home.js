b0c4d26281114017249e6c2aec5b2f89643d5439
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-07-08 20:54:31 -0700

    added styling to homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 class Home extends Component {
   render() {
     return (
-      <View>
-        <Text>Plant Momma</Text>
+      <View style={styles.mainHomepageContainer}>
+        <Text style={styles.titleStyle}>Plant Momma</Text>
+
+        <Image 
+          style={styles.imageStyle}
+          source={require('../images/adansonii-transparent.png')}
+        />
 
         <Button 
             title="My Plants"
@@ -17,14 +22,51 @@ class Home extends Component {
             onPress={ () => this.props.navigation.navigate('AddPlant') }
         />
 
-        <Button 
-          title="About Plant Mamma"
-          onPress={ () => this.props.navigation.navigate('AboutApp') }
-        />
+        <View style={styles.aboutAppStyle}>
+          <TouchableOpacity 
+            onPress={ () => this.props.navigation.navigate('AboutApp') }
+          >
+            <Text style={styles.aboutAppButtonText}>About Plant Momma</Text>
+          </TouchableOpacity>
+        </View>
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainHomepageContainer: {
+    flex: 1,
+    backgroundColor: '#BDE1C9'
+  },
+  titleStyle: {
+      fontSize: 50,
+      color: '#055607',
+      fontWeight: 'bold',
+      alignSelf: 'center',
+      paddingTop: 10
+  },
+  imageStyle: {
+    width: 85, 
+    height: 85,
+    alignSelf: 'center',
+    marginTop: 30,
+    marginBottom: 30
+  },
+  aboutAppStyle: {
+    flex: 1,
+    justifyContent: 'center', 
+    alignItems: 'center',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+  aboutAppButtonText: {
+    fontSize: 15,
+    color: '#055607'
+  }
+});
 
 export default Home;
 
