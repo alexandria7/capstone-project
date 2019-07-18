32073da494e4eaf097513388b36b783445bd7401
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-07-18 16:26:05 -0700

    commented out error for now when trying to render list of discussion items

--- src/screens/MyConversations.js
+++ src/screens/MyConversations.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import firebase from 'firebase';
+import _ from 'lodash';
 
 class MyConversations extends Component {
     constructor(props) {
         super(props);
     
+        this.state = {
+            allDiscussions: [],
+            startedDiscussions: [],
+            commentedDiscussions: []
+        }
     }
+
+    componentDidMount() {
+        this.getDiscussionsFromFirebase();
+    }
+
+    getDiscussionsFromFirebase = () => {
+        firebase.database().ref(`/discussions`)
+            .on('value', snapshot => {
+                console.log(snapshot.val());
+                const allDiscussions = _.map(snapshot.val(), (discussionObject, key) => {
+                    discussionObject.key = key;
+                    return discussionObject;
+                });
+                console.log('all discussions:', allDiscussions)
+
+                this.setState({allDiscussions});
+
+                // allDiscussions.forEach((post) => {
+                //     if (post["userId"] === firebase.auth().currentUser.uid) {
+                //         let currentPostList = this.state.startedDiscussions;
+                //         currentPostList.push(post);
+                //         this.setState({startedDiscussions: currentPostList})
+                //     }
+                // })
+            })
+
+            // this.createWrittenPostsList();
+            // .then(() => {
+            //     this.state.allDiscussions.forEach((post) => {
+            //         if (post["userId"] === firebase.auth().currentUser.uid) {
+            //             let currentPostList = this.state.startedDiscussions;
+            //             currentPostList.push(post);
+            //             this.setState({startedDiscussions: currentPostList})
+            //         }
+            //     })
+                // this.createWrittenPostsList();
+            // })
+    }
+
+    // createWrittenPostsList = () => {
+    //     this.state.allDiscussions.forEach((post) => {
+    //         if (post["userId"] === firebase.auth().currentUser.uid) {
+    //             let currentPostList = this.state.startedDiscussions;
+    //             currentPostList.push(post);
+    //             this.setState({startedDiscussions: currentPostList})
+    //         }
+    //     })
+    // }
+
     render() {
+
+        // const writtenPosts = this.state.allDiscussions.map((post, i) => {
+        //     <TouchableOpacity 
+        //         onPress={ () => console.log('post title was pressed!') }
+        //         // style={styles.plantContainerStyle}
+        //         key={i}
+        //     >
+        //         <Text>{post["question"]}</Text>
+        //     </TouchableOpacity> 
+        // });
+
         return (
             <View style={styles.aboutAppMainStyle}>
                 <View style={styles.headerStyle}>
@@ -20,6 +87,24 @@ class MyConversations extends Component {
                     </TouchableOpacity>
 
                     <Text style={styles.headerText}>Wet Your Plants</Text>
+                </View>
+
+                <View style={styles.mainPageSection}>
+                    <Text style={styles.mainTitleStyle}>Your Conversations</Text>
+
+                    <View style={styles.mainPostViewingSection}>
+                        <View>
+                            <Text style={styles.postSectionHeader}>Posts You've Written</Text>
+                            {/* { this.state.allDiscussions.length !== 0 ? 
+                                {writtenPosts} :
+                                <Text>You haven't written any posts yet.</Text>
+                            } */}
+                        </View>
+
+                        <View>
+                            <Text style={styles.postSectionHeader}>Posts You've Commented On</Text>
+                        </View>
+                    </View>
                 </View>
             </View>
         )
@@ -55,6 +140,25 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginTop: 10
     }, 
+    mainPageSection: {
+        paddingTop: 20,
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    mainTitleStyle: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: '#055607',
+        textAlign: 'center',
+    },
+    mainPostViewingSection: {
+        paddingTop: 15
+    },
+    postSectionHeader: {
+        fontWeight: 'bold',
+        fontSize: 20
+    }
 })
 
 export default MyConversations;
